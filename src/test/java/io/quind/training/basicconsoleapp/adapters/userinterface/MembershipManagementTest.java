44caa85c9a3c448f1e371f2db0e5b65af0b61b84
Usuario 307 <Usuario.307@estudiantes.uco>
2019-08-15 11:47:38 -0500

    new test

--- src/test/java/io/quind/training/basicconsoleapp/adapters/userinterface/MembershipManagementTest.java
+++ src/test/java/io/quind/training/basicconsoleapp/adapters/userinterface/MembershipManagementTest.java
@@ -1,16 +1,19 @@
 package io.quind.training.basicconsoleapp.adapters.userinterface;
 
+import io.quind.training.basicconsoleapp.adapters.persistence.FileHandler;
 import io.quind.training.basicconsoleapp.domain.Member;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
 
 import java.util.LinkedList;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -34,11 +37,20 @@ class MembershipManagementTest {
         assertEquals(expected, result);
         String resultAdd = mm.addMembers(buildList());
         assertEquals(expected,result);
+        mm.removeMember(buildList());
+        mm.printMemberInfo(buildList());
+        FileHandler fileHandler= new FileHandler();
+        fileHandler.appendFile(new Member(5,'1',"sdads",234523.0D));
+        fileHandler.overwriteFile(buildList());
 
     }
 
     private LinkedList<Member> buildList() {
         LinkedList<Member> list = new LinkedList<>();
+        Member member1 = new Member(5,'1',"sdads",234523.0D);
+        Member member2 = new Member(5,'1',"sdads",234523.0D);
+        list.add(member1);
+        list.add(member2);
         return list;
     }
 
